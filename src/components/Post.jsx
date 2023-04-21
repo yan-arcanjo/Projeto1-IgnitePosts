c5cbcf147bcc09f51d5cb53c4bfa02022f41a563
Yan <yanarcanjo.dev@gmail.com>
2023-04-20 21:40:36 -0300

    finalizado sidebar e iniciado componente de posts

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,8 +1,25 @@
+import styles from "./Post.module.css";
+
 export const Post = (props) => {
 	return (
-		<div>
-			<strong>{props.author}</strong>
-			<p>{props.content}</p>
-		</div>
+		<article className={styles.post}>
+			<header>
+				<div className={styles.author}>
+					<img
+						className={styles.avatar}
+						src="https://github.com/yan-arcanjo.png"
+						alt="profile picture"
+					/>
+					<div className={styles.authorInfo}>
+						<strong>Yan Arcanjo</strong>
+						<span>Web Developer</span>
+					</div>
+				</div>
+
+				<time title="11 de maio às 08:13" dateTime="2022-05-11 08:13:30">
+					Públicado há 1h
+				</time>
+			</header>
+		</article>
 	);
 };
